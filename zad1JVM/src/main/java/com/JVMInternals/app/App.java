be10b061b14ddbc5b0b35caad9dd66683491b5ec
Shloogie <lukermaker11@wp.pl>
2017-02-24 11:47:46 +0100

    zadanie 1

---
+++ zad1JVM/src/main/java/com/JVMInternals/app/App.java
@@ -0,0 +1,13 @@
+package com.JVMInternals.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ zad1JVM/src/main/java/com/JVMInternals/app/PeN.java
@@ -0,0 +1,15 @@
+package com.JVMInternals.app;
+
+import java.lang.*;
+
+public class PeN {
+    
+    static final int size = 1024 * 1024 * 1024;
+
+    public static void main(String[] args) {
+
+      int[] ig = new int[size];
+      
+  }
+    
+}
